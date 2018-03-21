026c8b6b7d6382fb13f6c8a67fad26a304f350bd
pofystyy <pofystyy@gmail.com>
2018-03-21 22:15:26 +0200

    fault rectification

--- main.rb
+++ main.rb
@@ -1,2 +1,37 @@
 require 'pg'
 require 'nokogiri'
+
+html_data = open('https://ain.ua').read
+nokogiri_object = Nokogiri::HTML(html_data)
+elements = nokogiri_object.css('.block-interesting').css('.block-interesting-list')
+
+# connection to the database
+con = PG.connect 
+	:dbname 	=> '***', 
+	:user 		=> '***', 
+  :password => '***'
+
+# create a database table
+con.exec "DROP TABLE IF EXISTS Posts"
+  con.exec "CREATE TABLE Posts(Id INTEGER PRIMARY KEY, 
+    Name VARCHAR(20), Views INT)"
+
+parsed = [] # array for parsed text
+count = 0
+while count < elements.css('.item-title').count
+	elements.each do |element|
+		parsed << count + 1 # post id
+		parsed << element.css('.item-title')[count].text # post name
+		parsed << element.css('.views')[count].text # post views
+
+=begin
+	
+		add to database
+		con.exec "INSERT INTO Posts VALUES(parsed[0], parsed[1], parsed[2])"
+
+		--- entering data in the database ---
+		
+=end
+		count += 1
+	end
+end
