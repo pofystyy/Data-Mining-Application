bc82bf8e229f8b855c6f982bc2ff25fb90c60d0e
pofystyy <pofystyy@gmail.com>
2018-12-31 23:27:04 +0200

    replacement of the pg database to the mongo

--- main.rb
+++ main.rb
@@ -1,35 +1,28 @@
-require 'pg'
+require 'mongo'
 require 'nokogiri'
 require 'open-uri'
 
 html_data = open('https://ain.ua').read
 nokogiri_object = Nokogiri::HTML(html_data)
-elements = nokogiri_object.css('.block-interesting').css('.block-interesting-list')
+elements = nokogiri_object.css('.interesting-now')
 
-# connection to the database
-con = PG.connect 
-	:dbname 	=> '***', 
-	:user 		=> '***', 
- 	:password 	=> '***'
+settings_line = ENV["SETTINGS_LINE"]
+database =  ENV["DATABASE"]
+@collection = ENV["COLLECTION"]
 
-# create a database table
-con.exec "DROP TABLE IF EXISTS Posts"
-  con.exec "CREATE TABLE Posts(Id INTEGER PRIMARY KEY, 
-    Name VARCHAR(150), Views INT)"
+Mongo::Logger.logger.level = ::Logger::FATAL
+@client = Mongo::Client.new([ settings_line ], database: database )
 
-parsed = [] # array for parsed text
+parsed = {}
 count = 0
 while count < elements.css('.item-title').count
-	elements.each do |element|
-		parsed << count + 1 # post id
-		parsed << element.css('.item-title')[count].text # post name
-		parsed << element.css('.views')[count].text # post views
+  elements.each do |element|
 
-		arr = []
-		arr << parsed.each_slice(3) { |f| arr << f } # sample of individual posts
+    parsed['name'] = element.css('.item-title')[count].text
+    parsed['views'] = element.css('.views')[count].text
 
-		con.exec "INSERT INTO Posts VALUES('#{arr[count][0]}', '#{arr[count][1]}', '#{arr[count][2]}')"
+    @client[@collection.to_sym].insert_one(parsed)
 
-		count += 1
-	end
+    count += 1
+  end
 end
